2a8f18c60e3be8ae694d15b31359042ed0cdbab6
Welton <weltonrdesigner@gmail.com>
2024-02-27 20:54:58 -0300

    Initial Commit

---
+++ Estudo-de-Classes/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudo_de_Classes.bin
+{
+    public class Pessoa
+    {
+        public string  nome { get; set; }
+        public int idade { get; set; }
+    }
+}
