9bc94281ffc423f625dd829fff2ce673185c93e9
Welton <weltonrdesigner@gmail.com>
2024-02-27 21:12:20 -0300

    Criando Classe

--- Estudo-de-Classes/Pessoa.cs
+++ Estudo-de-Classes/Pessoa.cs
@@ -7,7 +7,11 @@ namespace Estudo_de_Classes.bin
 {
     public class Pessoa
     {
-        public string  nome { get; set; }
-        public int idade { get; set; }
+        public string  Nome { get; set; }
+        public int Idade { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Oi, me chamo {Nome} e tenho {Idade} anos de idade!");
+        }
     }
 }
--- Estudo-de-Classes/Program.cs
+++ Estudo-de-Classes/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Estudo_de_Classes.bin;
+
+Console.WriteLine("Instanciando a classe pessoa e criando um objeto");
+Pessoa p1 = new Pessoa();
+p1.Nome = "Welton";
+p1.Idade = 32;
+p1.Apresentar();
+Console.ReadKey();
